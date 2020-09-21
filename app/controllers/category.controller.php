3aa9ed3536e560987099eaa6972601f114e6395c
Gabriel Ballone <gabrielgeno@gmail.com>
2020-09-21 18:12:34 -0300

    Desarrollo del controlador de categorías

--- app/controllers/category.controller.php
+++ app/controllers/category.controller.php
@@ -1,4 +1,48 @@
 <?php
-/**
- *   TODO controlador de categorías
- */
+include_once 'app/models/course.model.php';
+include_once 'app/models/category.model.php';
+include_once 'app/views/category.view.php';
+
+class CategoryController {
+
+    private $model;
+    private $modelCourse;
+    private $view;
+
+    function __construct() {
+        $this->model = new CategoryModel();
+        $this->modelCourse = new CourseModel();
+        $this->view = new CourseView();
+    }
+
+
+    function show(){ 
+        //parametros: cursos ,  , cursos/:id
+        if(isset($params[1])){
+            $this->showCategory($params[1]);
+        }
+        else{
+            $this->showCategories();
+        }
+    }
+ 
+    function showCategories() {
+        // obtiene las tareas del modelo
+        $categories = $this->model->getAll();
+        // actualizo la vista
+        $this->view->showCategory($categories);
+    }   
+
+    function showCategory() {
+    /*
+    ** PARA REVISAR. Debe recibir parametros. Uso de getAll
+    */
+        // obtiene las tareas del modelo
+        $category = $this->modelCategory->getAll();
+
+       // actualizo la vista
+       $this->view->showCategory($category);
+    }
+
+    
+}
